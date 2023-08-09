b0f65dcafb67d875a6db5bda47fbd7456d094ccf
nbogie <neillbogie@googlemail.com>
2023-08-09 22:27:25 +0100

    formatting?

--- src/mainSketch/mainSketch.ts
+++ src/mainSketch/mainSketch.ts
@@ -1,12 +1,12 @@
-import p5 from "p5";
-import { io } from "socket.io-client";
-import { generateCameraShakeVector, updateCamera } from "./cameraShake";
-import { Cloud, drawClouds, setupClouds, updateClouds } from "./clouds";
-import { drawDucks, setupDucks, updateDucks } from "./ducks";
-import { drawDustParticles, updateDustParticles } from "./dust";
-import { drawExplosions, updateExplosions } from "./explosions";
-import { drawGround } from "./ground";
-import { drawPowerups, setupPowerups, updatePowerups } from "./powerups";
+import p5 from 'p5';
+import { io } from 'socket.io-client';
+import { generateCameraShakeVector, updateCamera } from './cameraShake';
+import { Cloud, drawClouds, setupClouds, updateClouds } from './clouds';
+import { drawDucks, setupDucks, updateDucks } from './ducks';
+import { drawDustParticles, updateDustParticles } from './dust';
+import { drawExplosions, updateExplosions } from './explosions';
+import { drawGround } from './ground';
+import { drawPowerups, setupPowerups, updatePowerups } from './powerups';
 import {
     Projectile,
     drawProjectiles,
@@ -15,11 +15,11 @@ import {
     processReceivedBullet,
     setupProjectileSounds,
     updateProjectiles,
-} from "./projectile";
-import { drawSky, setupSky, updateSky } from "./sky";
-import { drawSun } from "./sun";
-import { ReceivedTank, Tank } from "./tank";
-import { WeaponSystem, setupWeaponSystem } from "./weaponSys";
+} from './projectile';
+import { drawSky, setupSky, updateSky } from './sky';
+import { drawSun } from './sun';
+import { ReceivedTank, Tank } from './tank';
+import { WeaponSystem, setupWeaponSystem } from './weaponSys';
 
 let config = {
     shouldTransmit: false,
@@ -27,7 +27,7 @@ let config = {
     includePowerups: true,
 };
 
-const socket = io("https://socketioserverc7demo.neillbogie.repl.co");
+const socket = io('https://socketioserverc7demo.neillbogie.repl.co');
 
 let seed = 123;
 
@@ -50,10 +50,10 @@ function setupSounds(p: p5) {
 
 function setupPalette() {
     const palette = {
-        grass: "rgb(82,180,82)",
-        dirt: "rgb(181,133,74)",
-        dust: "rgba(187,151,60,0.82)",
-        cloud: "rgba(255,255,255,0.22)",
+        grass: 'rgb(82,180,82)',
+        dirt: 'rgb(181,133,74)',
+        dust: 'rgba(187,151,60,0.82)',
+        cloud: 'rgba(255,255,255,0.22)',
     };
     return palette;
 }
@@ -64,59 +64,59 @@ export function getPaletteColour(key: keyof Palette): string {
 }
 
 function registerSocketListeners() {
-    socket.on("newClientStart", () => {});
-    socket.emit("newClientStart");
-    socket.on("tankUpdate", processReceivedTank);
-    socket.on("bulletFired", processReceivedBullet);
+    socket.on('newClientStart', () => {});
+    socket.emit('newClientStart');
+    socket.on('tankUpdate', processReceivedTank);
+    socket.on('bulletFired', processReceivedBullet);
 }
 
 function drawHUDText(p: p5) {
     p.textSize(20);
     p.text(
-        "Cached tank ids: " +
+        'Cached tank ids: ' +
             Object.keys(cachedTanks)
-                .map((id: string) => "..." + id.slice(-4))
-                .join(", "),
+                .map((id: string) => '...' + id.slice(-4))
+                .join(', '),
         100,
         120
     );
     p.text(
-        config.shouldTransmit ? "transmit is on (t)" : "transmit is off (t)",
+        config.shouldTransmit ? 'transmit is on (t)' : 'transmit is off (t)',
         100,
         150
     );
-    p.text("Weapon: " + (weaponSystem.canFire() ? "OK" : "..."), 100, 180);
-    p.text("ammo: " + "*".repeat(weaponSystem.getAmmoCount()), 100, 210);
+    p.text('Weapon: ' + (weaponSystem.canFire() ? 'OK' : '...'), 100, 180);
+    p.text('ammo: ' + '*'.repeat(weaponSystem.getAmmoCount()), 100, 210);
 }
 
 function loadImages(p: p5) {
     for (let i = 0; i < 5; i++) {
-        let greyPath = "tanks_tankGrey" + p.str(i + 1) + ".png";
-        let imageGrey = p.loadImage("/images/" + greyPath);
+        let greyPath = 'tanks_tankGrey' + p.str(i + 1) + '.png';
+        let imageGrey = p.loadImage('/images/' + greyPath);
         tankImgs[i] = imageGrey;
         // let navyPath = 'tanks_tankNavy' + p.str(i + 1) + '.png';
         // let imageNavy = p.loadImage('/images/' + navyPath);
         // tankImgs[i] = imageNavy;
     }
 
-    images.bullet = p.loadImage("/images/" + "tank_bullet2.png");
-    images.shield = p.loadImage("/images/" + "shield.png");
-    images.arrowEmpty = p.loadImage("/images/" + "tank_arrowEmpty.png");
-    images.arrowFull = p.loadImage("/images/" + "tank_arrowFull.png");
-    images.crateWood = p.loadImage("/images/" + "tanks_crateWood.png");
-    images.crateArmor = p.loadImage("/images/" + "tanks_crateArmor.png");
-    images.crateAmmo = p.loadImage("/images/" + "tanks_crateAmmo.png");
-    images.crateRepair = p.loadImage("/images/" + "tanks_crateRepair.png");
-    turretImg = p.loadImage("/images/" + "turret2.png");
-    images.duck1 = p.loadImage("/images/" + "duck_outline_target_brown.png");
-    images.duck2 = p.loadImage("/images/" + "duck_outline_target_white.png");
-    images.duck3 = p.loadImage("/images/" + "duck_outline_target_yellow.png");
-    images.duckStick = p.loadImage("/images/" + "stick_wood.png");
+    images.bullet = p.loadImage('/images/' + 'tank_bullet2.png');
+    images.shield = p.loadImage('/images/' + 'shield.png');
+    images.arrowEmpty = p.loadImage('/images/' + 'tank_arrowEmpty.png');
+    images.arrowFull = p.loadImage('/images/' + 'tank_arrowFull.png');
+    images.crateWood = p.loadImage('/images/' + 'tanks_crateWood.png');
+    images.crateArmor = p.loadImage('/images/' + 'tanks_crateArmor.png');
+    images.crateAmmo = p.loadImage('/images/' + 'tanks_crateAmmo.png');
+    images.crateRepair = p.loadImage('/images/' + 'tanks_crateRepair.png');
+    turretImg = p.loadImage('/images/' + 'turret2.png');
+    images.duck1 = p.loadImage('/images/' + 'duck_outline_target_brown.png');
+    images.duck2 = p.loadImage('/images/' + 'duck_outline_target_white.png');
+    images.duck3 = p.loadImage('/images/' + 'duck_outline_target_yellow.png');
+    images.duckStick = p.loadImage('/images/' + 'stick_wood.png');
 }
 
-function keyPressed(event: any, p: p5) {
-    console.log("key pressed: ", { event, p });
-    if (p.key === " ") {
+function keyPressed(_event: any, p: p5) {
+    // console.log("key pressed: ", { event, p });
+    if (p.key === ' ') {
         if (player.isDead) {
             return;
         }
@@ -125,7 +125,7 @@ function keyPressed(event: any, p: p5) {
             emitProjectile(projectile);
         }
     }
-    if (p.key === "t") {
+    if (p.key === 't') {
         config.shouldTransmit = !config.shouldTransmit;
     }
 }
@@ -210,7 +210,7 @@ function createSketch(p: p5) {
 
     function handleMousePressed() {
         if (p.mouseButton === p.LEFT) {
-            p.background("white");
+            p.background('white');
         }
     }
 

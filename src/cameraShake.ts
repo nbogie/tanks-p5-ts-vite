6e6fe06996a67d994c0a4ad9381e93fdd3d0f40e
nbogie <neillbogie@googlemail.com>
2023-08-10 21:48:44 +0100

    formatting only

--- src/cameraShake.ts
+++ src/cameraShake.ts
@@ -1,15 +1,15 @@
-import p5 from "p5";
+import p5 from 'p5';
 
 let camShakeMagnitude = 0;
 
 export function shakeCamera() {
-	camShakeMagnitude += 10;
+    camShakeMagnitude += 10;
 }
 
-export function updateCamera(p:p5) {
-	camShakeMagnitude = p.max(0, camShakeMagnitude - 0.4);
+export function updateCamera(p: p5) {
+    camShakeMagnitude = p.max(0, camShakeMagnitude - 0.4);
 }
 
 export function generateCameraShakeVector() {
-	return p5.Vector.random2D().mult(camShakeMagnitude)
+    return p5.Vector.random2D().mult(camShakeMagnitude);
 }
